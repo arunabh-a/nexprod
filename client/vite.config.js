4b16f8cd1feb5305f9a6a501e57eede9b8d2ea96
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2025-01-24 14:51:29 +0530

    Server Interface Migration

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     server: {
         proxy: {
         '/api': {
-            target: 'http://localhost:5000',
+            target: `https://nexprod-interface.onrender.com`,
+            // target: `http://localhost:5000`,
         },
         },
     },
--- server/interface.js
+++ server/interface.js
@@ -2,21 +2,23 @@ import express from 'express';
 import { connectDB } from './config/db-config.js';
 import dotenv from 'dotenv';
 import productRoutes from './routes/product.routes.js';
-
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     connectDB();
-    console.log("Server started at http://localhost:" + port);
+    console.log("Server started at Render/:" + port);
 });
 
 
